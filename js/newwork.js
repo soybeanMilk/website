02c7527ba2913d2d9950a958315b57772e70eb84
zhangle <zl646371427@qq.com>
2016-06-14 19:54:27 +0800

    last

--- js/newwork.js
+++ js/newwork.js
@@ -64,6 +64,8 @@ $(".danghang_congtent_third").mouseover(function(event) {
 });
 
 
+
+
 $(".second_left_input").keyup(function() {
     var data = $(".second_left_input").val();
     if(data==""){
@@ -74,6 +76,87 @@ $(".second_left_input").keyup(function() {
 });
 
 
+
+var example1 = new Vue({
+  el: '#third_line_centet_fuck',
+  data: {
+    items: [
+      { kind: "content",message: '天猫' },
+      { kind: "content",message: '聚划算' },
+      { kind: "content",message: '天猫超市' },
+      { kind: "verticalline",message: '|' },
+      { kind: "content1",message: '淘抢购' },
+      { kind: "content1",message: '电器城' },
+      { kind: "content1",message: '司法拍卖' },
+      { kind: "content1",message: '中国质造' },
+      { kind: "content1",message: '特色中国' },
+      { kind: "verticalline",message: '|' },
+      { kind: "content1",message: '阿里旅行' },
+      { kind: "content1",message: '智能生活' },
+      { kind: "content1",message: '苏宁易购' },
+    ]
+  }
+})
+
+
+var example5 = new Vue({
+  el: '.twelve',
+  data: {
+    items: [
+        { src: "img/31.jpg",message: '充话费' },
+        { src: "img/32.jpg",message: '游戏' },
+        { src: "img/33.jpg",message: '旅行' },
+        { src: "img/34.jpg",message: '保险' },
+        { src: "img/35.jpg",message: '彩票' },
+        { src: "img/36.jpg",message: '电影' },
+        { src: "img/37.jpg",message: '点外卖' },
+        { src: "img/38.jpg",message: '理财' },
+        { src: "img/39.jpg",message: '找服务' },
+        { src: "img/310.jpg",message: '音乐' },
+        { src: "img/311.jpg",message: '水电煤' },
+        { src: "img/312.jpg",message: '火车票' },
+
+    ]
+  }
+})
+
+
+
+var example2 = new Vue({
+  el: '#third_line_center_center_fuck',
+  data: {
+    items: [
+        { src: "img/5.jpg"},
+        { src: "img/1.jpg"},
+        { src: "img/2.jpg"},
+        { src: "img/3.jpg"},
+        { src: "img/4.jpg"},
+        { src: "img/5.jpg"},
+        { src: "img/1.jpg"},
+
+    ]
+  }
+})
+
+var example10 = new Vue({
+  el: '.picture_12',
+  data: {
+    items: [
+        { src: "img/71.jpg"},
+        { src: "img/72.jpg"},
+        { src: "img/73.jpg"},
+        { src: "img/74.jpg"},
+        { src: "img/75.jpg"},
+        { src: "img/76.jpg"},
+        { src: "img/77.jpg"},
+        { src: "img/78.jpg"},
+        { src: "img/79.jpg"},
+        { src: "img/710.jpg"},
+
+
+    ]
+  }
+})
 
 
 var jilu;
@@ -146,6 +229,144 @@ $(".four_content_ul1").mouseout(function(event) {
 });
 
 
+var example3 = new Vue({
+  el: '#third_line_center_bottom_fuck',
+  data: {
+    items: [
+        {
+            id:"forth_kind",
+            imglist:[
+                {src: "img/261.jpg"},
+                {src: "img/262.jpg"},
+            ]
+        },
+        {
+            id:"first_kind",
+            imglist:[
+                {src: "img/211.jpg",width:"33.3%",height:"100%"},
+                {src: "img/212.jpg",width:"33.3%",height:"50%"},
+                {src: "img/213.jpg",width:"33.3%",height:"50%"},
+                {src: "img/214.jpg",width:"33.3%",height:"50%"},
+                {src: "img/215.jpg",width:"33.3%",height:"50%"},
+
+            ]
+
+        },
+        {
+            id:"second_kind",
+            imglist:[
+                {src: "img/221.jpg"},
+                {src: "img/222.jpg"},
+                {src: "img/223.jpg"},
+                {src: "img/224.jpg"},
+                {src: "img/225.jpg"},
+                {src: "img/226.jpg"},
+
+            ]
+
+        },
+        {
+            id:"second_kind",
+            imglist:[
+                {src: "img/231.jpg"},
+                {src: "img/232.jpg"},
+                {src: "img/233.jpg"},
+                {src: "img/234.jpg"},
+                {src: "img/235.jpg"},
+                {src: "img/236.jpg"},
+
+            ]
+
+        },
+        {
+            id:"second_kind",
+            imglist:[
+                {src: "img/241.jpg"},
+                {src: "img/242.jpg"},
+                {src: "img/243.jpg"},
+                {src: "img/244.jpg"},
+                {src: "img/245.jpg"},
+                {src: "img/246.jpg"},
+
+            ]
+        },
+        {
+            id:"third_kind",
+            imglist:[
+                {src: "img/251.jpg"},
+                {src: "img/252.jpg"},
+                {src: "img/253.jpg"},
+                {src: "img/254.jpg"},
+                {src: "img/255.jpg"},
+                {src: "img/256.jpg"},
+                {src: "img/257.jpg"},
+                {src: "img/258.jpg"},
+                {src: "img/259.jpg"},
+                {src: "img/2510.jpg"},
+                {src: "img/2511.jpg"},
+                {src: "img/2512.jpg"},
+            ]
+        },
+        {
+            id:"forth_kind",
+            imglist:[
+                {src: "img/261.jpg"},
+                {src: "img/262.jpg"},
+            ]
+        },
+        {
+            id:"first_kind",
+            imglist:[
+                {src: "img/211.jpg",width:"33.3%",height:"100%"},
+                {src: "img/212.jpg",width:"33.3%",height:"50%"},
+                {src: "img/213.jpg",width:"33.3%",height:"50%"},
+                {src: "img/214.jpg",width:"33.3%",height:"50%"},
+                {src: "img/215.jpg",width:"33.3%",height:"50%"},
+
+            ]
+
+        },
+    ]
+  }
+})
+
+
+var fuck = new Vue({
+  el:'.four_content_ul1',
+  data:{
+    item1:[
+        {
+            title:[
+            {biaoti:"充话费"},
+            {biaoti:"冲流量"},
+            {biaoti:"冲固话"},
+            {biaoti:"冲宽带"},
+
+            ],
+            content:[
+                {
+                    qian:[
+                        {jutihuafei:"100"},
+                        {jutihuafei:"10"},
+                        {jutihuafei:"200"},
+                        {jutihuafei:"20"},
+                        {jutihuafei:"300"},
+                        {jutihuafei:"30"},
+                        {jutihuafei:"500"},
+                        {jutihuafei:"50"},
+                        {jutihuafei:"1000"},
+
+
+                    ]
+                }
+            ]
+
+
+        }
+    ]
+  }
+
+})
 
 
 $(".incanghuafeili").click(function(event) {
@@ -200,6 +421,62 @@ $(".jiage").click(function(event) {
        $(".inhangdehuafei").hide(); 
     }
 });
+
+
+var example3 = new Vue({
+  el: '#third_line_center_bottom_fuck1',
+  data: {
+    items: [
+        {
+            display:"block",
+            imglist:[
+                {content: "阿里巴巴集团与哈萨克斯坦达成战略合作",width:"100%",color:"#FF2400"},
+                {content: "全球领导力人才招募",width:"50%"},
+                {content: "第三届县域电商峰会",width:"50%"},
+            ]
+        },
+        {
+            display:"none",    
+            imglist:[
+                {content: "出售假冒商品规则变更",width:"50%",color:"#FF2400"},
+                {content: "商品发布数量限制变更",width:"50%"},
+                {content: "中国质造市场管理规范",width:"50%"},
+                {content: "淘宝网营销规则变更",width:"50%"},
+            ]
+        },
+        {
+            display:"none",    
+            imglist:[
+                {content: "淘宝行业规划发布会",width:"50%",color:"#FF2400"},
+                {content: "阿里推出兼职神器",width:"50%"},
+                {content: "原创设计女装乐往",width:"50%"},
+                {content: "是赚钱还是骗子",width:"50%"},
+            ]
+        },
+        {
+            display:"none",    
+            imglist:[
+                {content: "淘宝用户必备神器",width:"50%",color:"#FF2400"},
+                {content: "卖家账户安全9守则",width:"50%"},
+                {content: "支付宝实名认证信息",width:"50%"},
+                {content: "账户没钱也被骗？",width:"50%"},
+            ]
+        },
+        {
+            display:"none",    
+            imglist:[
+                {content: "阿里联合公益计划启动",width:"50%",color:"#FF2400"},
+                {content: "一图看懂联合公益计划",width:"50%"},
+                {content: "公益宝贝卖家发票索取",width:"50%"},
+                {content: "公益机构淘宝开店攻略",width:"50%"},
+            ]
+        },
+  
+    ]
+  }
+})
+
+
 
 
 
